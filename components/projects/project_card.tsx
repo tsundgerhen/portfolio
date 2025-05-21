a7076a314f950abdfec45c0ab6e09a3bb3878aaf
Tsundgerhen <tergelb0528@gmail.com>
2025-05-21 16:20:03 +0800

    fix: project cardiin technologiin hesegt iluu comma avaj baisniig yanzlav

--- components/projects/project_card.tsx
+++ components/projects/project_card.tsx
@@ -3,7 +3,7 @@
 import { Box, Text, Button } from '@chakra-ui/react';
 import { useRouter } from 'next/navigation';
 
-import {Project} from "@/types/index"
+import { Project } from '@/types/index';
 
 interface ProjectCardProps {
   projectInfo: Project;
@@ -14,24 +14,59 @@ export default function ProjectCard({ projectInfo, small_project }: ProjectCardP
   const router = useRouter();
 
   return (
-    <Box display={'block'} boxDirection={'column'} border={'1px solid var(--gray)'} w={{base:"90%", md:'25vw'}}>
+    <Box
+      display={'block'}
+      boxDirection={'column'}
+      border={'1px solid var(--gray)'}
+      w={{ base: '90%', md: '25vw' }}>
       {/* Image Section */}
       {projectInfo.image_src && <img src={projectInfo.image_src} alt={projectInfo.name} />}
 
-      <Text borderBottom={'1px solid var(--gray)'} borderTop={'1px solid var(--gray)'} padding={'12px'} fontFamily={'var(--font-family)'} color={'var(--gray)'}>
+      <Text
+        borderBottom={'1px solid var(--gray)'}
+        borderTop={'1px solid var(--gray)'}
+        padding={'12px'}
+        fontFamily={'var(--font-family)'}
+        color={'var(--gray)'}>
         {/* Display languages, tools, and frameworks */}
-        {projectInfo.languages.join(', ')}, {projectInfo.frameworks?.join(', ')}, {projectInfo.tools.join(', ')}
+        {[
+          projectInfo.languages?.join(', '),
+          projectInfo.frameworks?.length ? projectInfo.frameworks.join(', ') : null,
+          projectInfo.tools?.join(', '),
+        ]
+          .filter(Boolean)
+          .join(', ')}
       </Text>
 
-      <Box display={'flex'} flexDir={'column'} alignItems={'start'} justifyContent={'start'} padding={'12px'} fontFamily={'var(--font-family)'} color={'var(--gray)'} gapY={"12px"}>
+      <Box
+        display={'flex'}
+        flexDir={'column'}
+        alignItems={'start'}
+        justifyContent={'start'}
+        padding={'12px'}
+        fontFamily={'var(--font-family)'}
+        color={'var(--gray)'}
+        gapY={'12px'}>
         <Text color={'var(--white)'} fontSize={'xl'} fontWeight={'bold'}>
           {projectInfo.name}
         </Text>
         <Text>{projectInfo.info}</Text>
 
-        <Box display={'flex'} flexDirection={'row'} alignItems={'center'} justifyContent={'center'} w={'100%'}>
+        <Box
+          display={'flex'}
+          flexDirection={'row'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          w={'100%'}>
           {/* Button to redirect */}
-          <Button bg='transparent' border='1px solid var(--primary)' color='var(--white)' _hover={{ cursor: 'pointer', bg: 'var(--primary)', color: 'white' }} padding={'4px'} paddingX={'18px'} onClick={() => window.open(projectInfo.link, '_blank', 'noopener,noreferrer')}>
+          <Button
+            bg='transparent'
+            border='1px solid var(--primary)'
+            color='var(--white)'
+            _hover={{ cursor: 'pointer', bg: 'var(--primary)', color: 'white' }}
+            padding={'4px'}
+            paddingX={'18px'}
+            onClick={() => window.open(projectInfo.link, '_blank', 'noopener,noreferrer')}>
             {projectInfo.link_type === 'Domain' ? 'Live  <~>' : 'GitHub <~>'}
           </Button>
         </Box>
